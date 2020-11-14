0177f710cbb285d4b455e29b192d34f51f0770b9
Mikheil Sakhelashvili <mishka.sakhelashvili@gmail.com>
2020-11-14 23:40:04 +0400

    demo add

---
+++ test.js
@@ -0,0 +1,17 @@
+// const sum = n => {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//       sum += i;
+//     }
+//     return sum;
+// }
+
+
+const sum = n => {
+  return n * (n + 1) / 2;
+}
+
+
+
+console.log(sum(9000000000 ))
+ 
